0bc286fa370c45f04935033fd3bba6c212f3797d
Marko <michinaux.marko@gmail.com>
2023-11-17 14:15:21 -0400

    testing webhook pushs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 exports.handler = async (event: any) => {
-	let body = JSON.parse(event.body)
-	const product = body.num1 * body.num2;
+	const body = JSON.parse(event.body);
+	
+	console.log('BODY ITEMS!');
+	console.log(event.body);
+	console.log('PARSED BODY ITEMS!');
+	console.log(JSON.stringify(body.line_items));
+
 	const response = {
 		statusCode: 200,
-		body: "The product of " + body.num1 + " and " + body.num2 + " is " + product,
+		body,
 	};
+
 	return response;
 };
